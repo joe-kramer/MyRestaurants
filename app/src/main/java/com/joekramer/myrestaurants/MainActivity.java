48dee673870aae7f98dc22d56ac3184eaecf2617
Joe Kramer <joekramer1125@gmail.com>
2017-09-06 14:49:09 -0700

    add OnClickListener interface and refactor

--- app/src/main/java/com/joekramer/myrestaurants/MainActivity.java
+++ app/src/main/java/com/joekramer/myrestaurants/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
@@ -29,14 +29,16 @@ public class MainActivity extends AppCompatActivity {
         Typeface oleoFont = Typeface.createFromAsset(getAssets(), "fonts/oleoscriptbold.ttf");
         mAppNameTextView.setTypeface(oleoFont);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
