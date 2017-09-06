a88accc60434aa2672c8a417aa6cc385147ae963
Joe Kramer <joekramer1125@gmail.com>
2017-09-06 13:15:03 -0700

    add click event on listview and tests

--- app/src/main/java/com/joekramer/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/joekramer/myrestaurants/RestaurantsActivity.java
@@ -3,9 +3,13 @@ package com.joekramer.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,6 +19,7 @@ import butterknife.ButterKnife;
 public class RestaurantsActivity extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
     private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
             "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
@@ -28,6 +33,14 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, restaurants);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
