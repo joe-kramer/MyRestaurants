fc4b8f08adaa0f882ba723538fd093587f76bacf
Joe Kramer <joekramer1125@gmail.com>
2017-09-06 10:12:34 -0700

    add text view to display zip on restaurants view

--- app/src/main/java/com/joekramer/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/joekramer/myrestaurants/RestaurantsActivity.java
@@ -1,13 +1,24 @@
 package com.joekramer.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
