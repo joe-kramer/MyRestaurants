294e0ad7e43000dfb2030d63af9c2ccef69002b8
Joe Kramer <joekramer1125@gmail.com>
2017-09-11 15:05:57 -0700

    clean by deleting yelp api fusion api information

--- app/src/main/java/com/joekramer/myrestaurants/Constants.java
+++ app/src/main/java/com/joekramer/myrestaurants/Constants.java
@@ -8,8 +8,4 @@ public class Constants {
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
-    public static final String YELP_CLIENT_ID = BuildConfig.YELP_CLIENT_ID;
-    public static final String YELP_CLIENT_SECRET = BuildConfig.YELP_CLIENT_SECRET;
-    public static final String YELP_ACCESS_TOKEN = BuildConfig.YELP_ACCESS_TOKEN;
-    public static final String YELP_TOKEN_TYPE = "Bearer ";
 }
