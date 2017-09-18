743b86db8bba31cd2d0fe5775bd4c902d5227d7d
Joe Kramer <joekramer1125@gmail.com>
2017-09-18 14:38:11 -0700

    add implicit intents to restaurant - call, location on google maps, navigate to website on chrome

--- app/src/main/java/com/joekramer/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/joekramer/myrestaurants/ui/RestaurantDetailFragment.java
@@ -1,6 +1,8 @@
 package com.joekramer.myrestaurants.ui;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,9 +20,10 @@ import org.parceler.Parcels;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class RestaurantDetailFragment extends Fragment {
+public class RestaurantDetailFragment extends Fragment implements View.OnClickListener {
     private static final int MAX_WIDTH = 400;
     private static final int MAX_HEIGHT = 300;
+
     @Bind(R.id.restaurantImageView) ImageView mImageLabel;
     @Bind(R.id.restaurantNameTextView) TextView mNameLabel;
     @Bind(R.id.cuisineTextView) TextView mCategoriesLabel;
@@ -50,7 +53,7 @@ public class RestaurantDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
         ButterKnife.bind(this, view);
 
         Picasso.with(view.getContext())
@@ -65,7 +68,31 @@ public class RestaurantDetailFragment extends Fragment {
         mPhoneLabel.setText(mRestaurant.getPhone());
         mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
 
+        mWebsiteLabel.setOnClickListener(this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener(this);
+
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteLabel) {
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+            Uri.parse(mRestaurant.getWebsite()));
+        startActivity(webIntent);
+        }
+        if (v == mPhoneLabel) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
+            Uri.parse("tel:" + mRestaurant.getPhone()));
+        startActivity(phoneIntent);
+        }
+        if (v == mAddressLabel) {
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,
+            Uri.parse("geo:" + mRestaurant.getLatitude()
+            + "," + mRestaurant.getLongitude()
+            + "?q=(" + mRestaurant.getName() + ")"));
+        startActivity(mapIntent);
+        }
+    }
 }
